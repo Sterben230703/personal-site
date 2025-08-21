5c4f3e6a2199051af2daf48808d8047d9e0a56be
Sterben230703 <anand230703@gmail.com>
2025-08-22 04:00:59 +0530

    deployment build fixes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,16 +3,15 @@ import { getPostBySlug } from '@/lib/posts';
 import Layout from '@/components/layout';
 import { notFound } from 'next/navigation';
 import { marked } from 'marked';
-
 interface PostPageProps {
   params: { slug: string };
 }
 
-export default function PostPage({ params }: PostPageProps) {
+export default async function PostPage({ params }: PostPageProps) {
   const { slug } = params;
   let post;
   try {
-    post = getPostBySlug(slug);
+    post = await getPostBySlug(slug);
   } catch {
     notFound();
   }
